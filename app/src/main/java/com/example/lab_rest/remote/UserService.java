6588f1b16f8d49bc5d4dda033071a337b664f1b6
waniafrina <2023273052@student.uitm.edu.my>
2025-06-25 13:48:13 +0800

    UserService.java update

--- app/src/main/java/com/example/lab_rest/remote/UserService.java
+++ app/src/main/java/com/example/lab_rest/remote/UserService.java
@@ -1,10 +1,13 @@
 package com.example.lab_rest.remote;
 
 import com.example.lab_rest.model.User;
+import com.example.lab_rest.model.Request;
 
+import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface UserService {
@@ -18,6 +21,9 @@ public interface UserService {
     @POST("users/login")
     public Call<User> loginEmail(@Field("email") String email,
                             @Field("password") String password);
+
+    @GET("requests")
+    Call<List<Request>> getAllRequests();
 
 
 }
