e9463b21e8b9dc87d62a7e8dbab9f1e66089e64a
kasyifah <2023406652@student.uitm.edu.my>
2025-06-26 01:01:56 +0800

    kasyi 5

--- app/src/main/java/com/example/lab_rest/UpdateItemActivity.java
+++ app/src/main/java/com/example/lab_rest/UpdateItemActivity.java
@@ -1,4 +1,52 @@
 package com.example.lab_rest;
 
-public class UpdateItemActivity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.lab_rest.sharedpref.SharedPrefManager;
+
+public class UpdateItemActivity extends AppCompatActivity {
+    private EditText txtItemId;
+    private EditText txtItemName;
+    private EditText txtPrice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_update_item);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // get view objects references
+        txtItemId = findViewById(R.id.txtItemId);
+        txtItemName = findViewById(R.id.txtItemName);
+        txtPrice = findViewById(R.id.txtPrice);
+
+    }
+
+    public void clearSessionAndRedirect() {
+        // clear the shared preferences
+        SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
+        spm.logout();
+
+        // terminate this MainActivity
+        finish();
+
+        // forward to Login Page
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+    }
 }
