fe0e1b64f236100cf0ad2c17000551cb1a2064e4
Tasha <2023663506@student.uitm.edu.my>
2025-06-29 23:10:23 +0800

    Commit

--- app/src/main/java/com/example/lab_rest/model/Item.java
+++ app/src/main/java/com/example/lab_rest/model/Item.java
@@ -4,6 +4,18 @@ public class Item {
     private int itemId;
     private String itemName;
     private double price;
+    //new
+    private int imageResId;
+    private int quantity = 0;
+
+
+    public Item(int itemId, String itemName, double price, int imageResId) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.price = price;
+        this.imageResId = imageResId;
+        this.quantity = 0;
+    }
 
     public int getItemId() {
         return itemId;
@@ -16,4 +28,17 @@ public class Item {
     public double getPrice() {
         return price;
     }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
+
