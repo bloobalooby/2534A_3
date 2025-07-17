6efb70280607f7347038898bc02d7f92871efc39
kasyifah <2023406652@student.uitm.edu.my>
2025-07-17 22:34:50 +0800

    request service

---
+++ app/src/main/java/com/example/lab_rest/remote/RequestService.java
@@ -0,0 +1,20 @@
+package com.example.lab_rest.remote;
+
+import com.example.lab_rest.model.Request;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
+public interface RequestService {
+    @GET("requests")
+    Call<List<Request>> getAllRequests(@Header("api-key") String api_key);
+
+    @GET("requests/{id}")
+    Call<Request> getRequests(@Header("api-key") String api_key, @Path("id") int id);
+
+
+}
